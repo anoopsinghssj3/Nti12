962b25e6206d29400962a08b1c45ff33005634a9
priyanka soni <157217093+priyankasoni19@users.noreply.github.com>
2025-06-13 17:12:09 +0530

    nti

--- src/anoop/ps.jsx
+++ src/anoop/ps.jsx
@@ -8,6 +8,13 @@ jsdhfaljsdhflaiwfhioawhflsaidhfaiushfoadshvlkjsdvhluisah;oias
 dkjvhsidvhlisdhvsd
 sdkfjvbhsdkjfhvilsd
 sd.fvnsdkjfhvijls
+<div className="btn"> edit</div>
+<div className="btn"> cancel</div>
+<div className="btn"> refresh</div>
+<div className="btn"> add </div>
+<div className="btn"> edit</div>
+<div className="btn"> edit</div>
+
     </div>
   )
 }
