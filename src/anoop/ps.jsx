8fc548fd26451f7de3ead127f1f28127f27f762f
Anoop Singh <anoopsinghssj3@gmail.com>
2025-06-13 17:35:42 +0530

    aadee

--- src/anoop/ps.jsx
+++ src/anoop/ps.jsx
@@ -5,10 +5,7 @@ export default function lorem50
   return (
     <div>
       <div className="bg-danger">
-        lorem50
-        kjdvnv d  dkvdiin   kidnieefwief fneiiene ewiiininnkijikijiljnhin
-      </div>
-      684af4ecde5a6134e15d9d575778f32af6f1b581
+        c sjk ksknak </div>
     </div>
   )
 }
