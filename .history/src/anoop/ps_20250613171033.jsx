c3de2bd50946b9ab8ded8d3fe94fd50b472a34b7
priyanka soni <157217093+priyankasoni19@users.noreply.github.com>
2025-06-14 15:31:06 +0530

    nti

---
+++ .history/src/anoop/ps_20250613171033.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function    lorem50
+() {
+  return (
+    <div>  
+jsdhfaljsdhflaiwfhioawhflsaidhfaiushfoadshvlkjsdvhluisah;oias
+dkjvhsidvhlisdhvsd
+sdkfjvbhsdkjfhvilsd
+sd.fvnsdkjfhvijls
+<div className="btn"> edit</div>
+
+
+    </div>
+  )
+}
---
+++ .history/src/anoop/ps_20250613171448.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function    lorem50
+() {
+  return (
+    <div>  
+jsdhfaljsdhflaiwfhioawhflsaidhfaiushfoadshvlkjsdvhluisah;oias
+dkjvhsidvhlisdhvsd
+sdkfjvbhsdkjfhvilsd
+sd.fvnsdkjfhvijls
+
+<div className="btn"> edit</div>
+<div className="btn"> cancel</div>
+<div className="btn"> refresh</div>
+<div className="btn"> add </div>
+<div className="btn"> edit</div>
+<div className="btn"> edit</div>
+
+
+
+
+<div className="bg-danger">
+  lorem50
+  kjdvnv d  dkvdiin   kidnieefwief fneiiene ewiiininnkijikijiljnhin
+</div>
+684af4ecde5a6134e15d9d575778f32af6f1b581
+    </div>
+  )
+}
