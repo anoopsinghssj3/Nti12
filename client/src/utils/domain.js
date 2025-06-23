07a453c85f2cef180b2d2624a0a677ae3087433b
Nitin2049 <nitinsingh2049@gmail.com>
2025-06-23 11:49:51 +0530

    domain changes 2

--- client/src/utils/domain.js
+++ client/src/utils/domain.js
@@ -1,2 +1,2 @@
 // export const domainRoute = "http://localhost:5000";
-export const domainRoute = "https://nti-tech-academy.onrender.com";
+export const domainRoute = "https://nti-tech-academy-server.onrender.com";
